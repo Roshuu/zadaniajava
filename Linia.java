33ddfbde33a5e519971e2c5fc304449d3a01220c
Roshuu <96373395+Roshuu@users.noreply.github.com>
2022-03-11 21:50:49 +0100

    Add files via upload

---
+++ Linia.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Linia {
+    Punkt poczatek;
+    Punkt koniec;
+
+    Linia(Punkt poczatek, Punkt koniec)
+    {
+        this.poczatek=poczatek;
+        this.koniec=koniec;
+    }
+
+
+}
