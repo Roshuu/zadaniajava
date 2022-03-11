1b02a243677450488844846bd566cf5f2ee409ae
Roshuu <96373395+Roshuu@users.noreply.github.com>
2022-03-11 21:59:14 +0100

    Add files via upload

---
+++ zadanie4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Pomoce.time();
+        Pomoce.dodajDwieLiczby(5,56);
+
+
+    }
+}
---
+++ zadanie4/src/com/company/Pomoce.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Pomoce {
+
+
+    public static void time()
+    {
+        System.out.println(LocalDate.now());
+    }
+
+    public static void dodajDwieLiczby(int x, int y)
+    {
+        System.out.println(x+y);
+    }
+
+
+
+
+}
